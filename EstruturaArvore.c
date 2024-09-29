b73feb4df2fc06c807a2d24c43a10800ecfaa06d
saulobmelo <157417082+SaulinhoDevs@users.noreply.github.com>
2024-09-29 13:03:59 -0300

    validar nivel

--- EstruturaArvore.c
+++ EstruturaArvore.c
@@ -3,61 +3,81 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct Node{
-        float num;
-        struct Node* esquerda;
-        struct Node* direita;
-}Node;
+typedef struct Node
+{
+    float num;
+    struct Node *esquerda;
+    struct Node *direita;
+} Node;
 
-Node* InseriroNo(Node *raiz, int numero) {
-        if(raiz == NULL){
-            return criarNo(numero)
-        }
+Node *InseriroNo(Node *raiz, int numero)
+{
+    if (raiz == NULL)
+    {
+        return criarNo(numero)
+    }
 
-        if(numero < raiz -> numero ){
-            raiz -> esquerda = InserirNo(raiz -> esquerda, numero);
+    if (numero < raiz->numero)
+    {
+        raiz->esquerda = InserirNo(raiz->esquerda, numero);
+    }
+    else if (numero < raiz->numero)
+    {
+        raiz->direita = InserirNo(raiz->direita, int numero);
+    }
 
-        }else if(numero < raiz -> numero){
-            raiz -> direita = InserirNo(raiz -> direita, int numero);
-
-        }
-
-        return raiz;
+    return raiz;
 }
 
-int main(){
+int altura(Node *raiz)
+{
+    if (raiz == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        int alturaEsq = altura(raiz->esquerda);
+        int alturaDir = altura(raiz->direita);
+        return (alturaEsq > alturaDir ? alturaEsq : alturaDir) + 1;
+    }
+}
+
+int main()
+{
     int opcao;
 
-    do {
+    do
+    {
         printf("( 0 ) Sair\n( 1 ) Insercao de NOS na arvore\n( 2 ) Remocao de NOS na arvore\n( 3 ) Ver altura da arvore\n( 4 ) Mostrar os elementos de um nivel\n( 5 ) Checar se arvore esta balanceada ou nao\n( 6 ) Mostrar estatistica da arvore\n");
-        scanf("%d",&opcao);
+        scanf("%d", &opcao);
 
         switch (opcao)
         {
-            case 1:
-                /* Inserção de NÓS na árvore */
-                break;
-            case 2:
-                /* Remoção de nós na árvore */
-                break;
-            case 3:
-                /* Ver altura da árvore */
-                break;
-            case 4:
-                /* Mostrar os elementos de um nível */
-                break;
-            case 5:
-                /* Checar se árvore está balanceada ou não */
-                break;
-            case 6:
-                /* Mostrar estatística da árvore */
-                break;
+        case 1:
+            /* Inserção de NÓS na árvore */
+            break;
+        case 2:
+            /* Remoção de nós na árvore */
+            break;
+        case 3:
+            /* Ver altura da árvore */
+            break;
+        case 4:
+            /* Mostrar os elementos de um nível */
+            break;
+        case 5:
+            /* Checar se árvore está balanceada ou não */
+            break;
+        case 6:
+            /* Mostrar estatística da árvore */
+            break;
 
-            default:
-                break;
+        default:
+            break;
         }
-    
-    } while( opcao != 0);
+
+    } while (opcao != 0);
 
     return 0;
 }
